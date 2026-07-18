2c7090ce6a4e980b835f857cb03ac3fca98b5f26
Thiago Heckler <thiag0224@hotmail.com>
2026-07-17 22:58:48 -0300

    Criação do projeto spring boot, configuração inicial para application.yml e schema.sql do banco, criação da entidade User

---
+++ src/main/java/com/thiago/taskapi/task_api/model/User.java
@@ -0,0 +1,74 @@
+package com.thiago.taskapi.task_api.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+	//Tabelas em java
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(nullable = false, length = 100)
+	private String name;
+	
+	@Column(nullable = false, unique = true)
+	private String email;
+	
+	@Column(name = "password_hash", nullable = false)
+	private String passwordHash;
+	
+	@Column(name = "created_at", insertable = false, updatable = false)
+	private LocalDateTime createdAt;
+	
+	protected User() {
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPasswordHash() {
+		return passwordHash;
+	}
+
+	public void setPasswordHash(String passwordHash) {
+		this.passwordHash = passwordHash;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+}
